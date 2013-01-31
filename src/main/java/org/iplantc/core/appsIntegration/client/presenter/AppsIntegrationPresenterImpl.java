00b69e289c7ede77324f662cfe3164c532898769
Jonathan Strootman <jstroot@iplantcollaborative.com>
2013-01-30 21:20:54 -0700

    Created a skeleton project for the Apps Integration module. This skeleton includes a basic configuration for the pom.xml and gwt.xml such that it plays nicely with the other projects. Additionally, a basic start to the MVP pattern has been set up.

---
+++ src/main/java/org/iplantc/core/appsIntegration/client/presenter/AppsIntegrationPresenterImpl.java
@@ -0,0 +1,19 @@
+package org.iplantc.core.appsIntegration.client.presenter;
+
+import org.iplantc.core.appsIntegration.client.view.AppsIntegrationView;
+
+import com.google.gwt.user.client.ui.HasOneWidget;
+
+/**
+ * @author jstroot
+ *
+ */
+public class AppsIntegrationPresenterImpl implements AppsIntegrationView.Presenter {
+
+    @Override
+    public void go(HasOneWidget container) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/main/java/org/iplantc/core/appsIntegration/client/view/AppsIntegrationView.java
@@ -0,0 +1,17 @@
+package org.iplantc.core.appsIntegration.client.view;
+
+/**
+ * @author jstroot
+ *
+ */
+public interface AppsIntegrationView {
+    
+    /**
+     * @author jstroot
+     *
+     */
+    public interface Presenter extends org.iplantc.core.uicommons.client.presenter.Presenter{
+        
+    }
+
+}
