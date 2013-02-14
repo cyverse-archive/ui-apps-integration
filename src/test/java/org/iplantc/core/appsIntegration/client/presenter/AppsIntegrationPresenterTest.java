89f43b4d12b622803dedc6e0adb64517e7d9fd63
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-02-13 17:46:52 -0700

    Removed default generated test failure

--- src/test/java/org/iplantc/core/appsIntegration/client/presenter/AppsIntegrationPresenterTest.java
+++ src/test/java/org/iplantc/core/appsIntegration/client/presenter/AppsIntegrationPresenterTest.java
@@ -1,14 +1,11 @@
 package org.iplantc.core.appsIntegration.client.presenter;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class AppsIntegrationPresenterTest {
 
     @Test
     public void test() {
-        fail("Not yet implemented");
     }
 
 }
