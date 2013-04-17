30b263a2d3cba97e8d718c1def283e6e7971f4c2
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-04-17 14:10:58 -0700

    IN PROGRESS - issue CORE-4152: Implement App Integration WYSIWYG Pallete Drag n' Drop https://pods.iplantcollaborative.org/jira/browse/CORE-4152
    Added DragSource capability to the images within AppIntegrationPalette.
    Now, on drag start, the drag source event data is loaded with the
    ArgumentType associated with the image being dragged. This allows any
    drop targets who are in the know have knowledge of what argument type
    control is intended to be added to the App wizard widget.

--- src/main/java/org/iplantc/core/uiapps/integration/client/view/AppIntegrationPalette.java
+++ src/main/java/org/iplantc/core/uiapps/integration/client/view/AppIntegrationPalette.java
@@ -1,19 +1,46 @@
 package org.iplantc.core.uiapps.integration.client.view;
 
+import org.iplantc.core.uiapps.widgets.client.dnd.AppArgumentTypeDragSource;
+import org.iplantc.core.uiapps.widgets.client.models.ArgumentType;
+
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Widget;
+import com.sencha.gxt.widget.core.client.Composite;
 
+/**
+ * This is a ui component which contains draggable images of the different supported argument types in
+ * the App Integration view.
+ * 
+ * @author jstroot
+ * 
+ */
 public class AppIntegrationPalette extends Composite {
 
     private static AppIntegrationPaletteUiBinder uiBinder = GWT.create(AppIntegrationPaletteUiBinder.class);
 
-    interface AppIntegrationPaletteUiBinder extends UiBinder<Widget, AppIntegrationPalette> {
-    }
+    interface AppIntegrationPaletteUiBinder extends UiBinder<Widget, AppIntegrationPalette> {}
+
+    @UiField
+    Image flag, environmentVariable, multiFileSelector, fileInput, group, integer, treeSelection, multiSelect, singleSelect, multiLineText, text;
 
     public AppIntegrationPalette() {
         initWidget(uiBinder.createAndBindUi(this));
+
+        // Add dragSource objects to each button
+        new AppArgumentTypeDragSource(environmentVariable, ArgumentType.EnvironmentVariable);
+        new AppArgumentTypeDragSource(fileInput, ArgumentType.FileInput);
+        new AppArgumentTypeDragSource(flag, ArgumentType.Flag);
+        new AppArgumentTypeDragSource(group, ArgumentType.Group);
+        new AppArgumentTypeDragSource(integer, ArgumentType.Integer);
+        new AppArgumentTypeDragSource(multiFileSelector, ArgumentType.MultiFileSelector);
+        new AppArgumentTypeDragSource(multiLineText, ArgumentType.MultiLineText);
+        new AppArgumentTypeDragSource(multiSelect, ArgumentType.TextSelection);
+        new AppArgumentTypeDragSource(text, ArgumentType.Text);
+        new AppArgumentTypeDragSource(singleSelect, ArgumentType.TextSelection);
+        new AppArgumentTypeDragSource(treeSelection, ArgumentType.TreeSelection);
     }
 
 }
